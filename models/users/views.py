22b0ed6b5e04ca8d797a416ce240108b3bc3abe4
jslvtr <jslvtr@gmail.com>
2016-01-01 18:49:10 +0000

    Some debugging changes to login.

--- models/users/views.py
+++ models/users/views.py
@@ -16,7 +16,7 @@ bp = Blueprint('user', __name__, url_prefix='/users')
 @bp.route('/login', methods=['POST', 'GET'])
 def login():
     log.info("Called /login endpoint, creating form")
-    form = LoginForm(request.form)
+    form = None #LoginForm(request.form)
     log.info("Form created, validating...")
     # make sure data are valid, but doesn't validate password is right
     if form.validate_on_submit():
@@ -24,7 +24,7 @@ def login():
         try:
             user = User.login(email=form.email.data,
                               password=form.password.data)
-            session['email'] = user.email
+            # session['email'] = user.email
             log.info("User logged in and e-mail in session.")
         except UserErrors.UserError as e:
             log.warn("User error with message '{}', redirecting to login".format(e.message))
@@ -32,7 +32,7 @@ def login():
         log.info("User logged in, redirecting to profile.")
         return redirect(url_for('.profile'))
     log.info("Form not valid or this is GET request, presenting users/login.html template")
-    return render_template('users/login.html', form=form)
+    return render_template('users/login.html')
 
 
 @bp.route('/register', methods=['POST', 'GET'])
